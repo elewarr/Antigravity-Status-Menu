6dd20cda3ad2b62684a0a523ab8c98018c11da5f
Krystian Lewandowski <k.lewandowski@icloud.com>
2026-01-16 23:58:11 +0100

    Initial Commit

---
+++ Antigravity Stats MenuTests/Antigravity_Stats_MenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  Antigravity_Stats_MenuTests.swift
+//  Antigravity Stats MenuTests
+//
+//  Created by Krystian Lewandowski on 16/01/2026.
+//
+
+import Testing
+@testable import Antigravity_Stats_Menu
+
+struct Antigravity_Stats_MenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
